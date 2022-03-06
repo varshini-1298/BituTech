c84567634a89e9b8ab9a6b91cf713c6a2c6992a5
Kannan-DCI <kannanp@paragondynamics.in>
2022-03-06 22:01:38 +0530

    Test Commit

--- src/main/java/com/bitutech/BituTechApp.java
+++ src/main/java/com/bitutech/BituTechApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class BituTechApp extends SpringBootServletInitializer {
 	public static void main(String[] args) {
+		System.out.println("BituTech App");
 		SpringApplication.run(BituTechApp.class, args);
 	}
 }
