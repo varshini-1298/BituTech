9e9bc91a30476f9154f1fe129f80e8c349a08c8d
Kannan-DCI <kannanp@paragondynamics.in>
2022-03-19 22:59:40 +0530

    Country and department master query changes

--- src/main/java/com/bitutech/currencyMaster/CurrencyMasterQueryUtil.java
+++ src/main/java/com/bitutech/currencyMaster/CurrencyMasterQueryUtil.java
@@ -3,6 +3,6 @@ package com.bitutech.currencyMaster;
 public class CurrencyMasterQueryUtil {
 
 	public static final String INSERT_CURRENCY_MASTER = null;
-	public static final String getCurrencyList = "select form_code as territory,form_name as name,display_order as zipCode,module_code as addressOfCus,is_parent as organisationName from auth.form";
+	public static final String getCurrencyList = "select form_code as currencyCode,form_name as currencyName from auth.form";
 
 }
--- src/main/java/com/bitutech/departmentmaster/DepartmentMasterQueryUtil.java
+++ src/main/java/com/bitutech/departmentmaster/DepartmentMasterQueryUtil.java
@@ -3,6 +3,6 @@ package com.bitutech.departmentmaster;
 public class DepartmentMasterQueryUtil {
 
 	public static final String INSERT_DEPARTMENT_MASTER = null;
-	public static final String getDepartmentList = "select form_code as territory,form_name as name,display_order as zipCode,module_code as addressOfCus,is_parent as organisationName from auth.form";
+	public static final String getDepartmentList = "select form_code as departmentCode,form_name as departmentName,display_order as departmentHead,module_code as profitCenter from auth.form";
 
 }
