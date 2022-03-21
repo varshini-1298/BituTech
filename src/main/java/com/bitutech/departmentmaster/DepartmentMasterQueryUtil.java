9998f70a3f5b37a767ab996fb16803332ff3ec62
Kannan-DCI <kannanp@paragondynamics.in>
2022-03-21 11:14:57 +0530

    master query changes

--- src/main/java/com/bitutech/departmentmaster/DepartmentMasterQueryUtil.java
+++ src/main/java/com/bitutech/departmentmaster/DepartmentMasterQueryUtil.java
@@ -3,6 +3,6 @@ package com.bitutech.departmentmaster;
 public class DepartmentMasterQueryUtil {
 
 	public static final String INSERT_DEPARTMENT_MASTER = null;
-	public static final String getDepartmentList = "select form_code as departmentCode,form_name as departmentName,display_order as departmentHead,module_code as profitCenter from auth.form";
+	public static final String getDepartmentList = "select dept_code as departmentCode,dept_name as departmentName,dept_head_code as departmentHead from department";
 
 }
