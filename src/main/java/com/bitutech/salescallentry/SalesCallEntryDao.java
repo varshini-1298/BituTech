554b5d234a58ca9ab10a8d94214f9957dbc3ef44
Kannan-DCI <kannanp@paragondynamics.in>
2022-03-23 18:28:13 +0530

    Revert "Revert "sales call entry list services""
    This reverts commit 5101df9a1d6bc204f8c12274739368b8218574af.

--- src/main/java/com/bitutech/salescallentry/SalesCallEntryDao.java
+++ src/main/java/com/bitutech/salescallentry/SalesCallEntryDao.java
@@ -1,7 +1,10 @@
 package com.bitutech.salescallentry;
 
+import java.util.List;
+
 public interface SalesCallEntryDao {
 
 	SalesCallEntryResultBean save(SalesCallEntryBean bean) throws Exception;
-
+	
+	List<SalesCallEntryBean> getList() throws Exception;
 }
