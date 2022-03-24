1e15743ed66b265d2c9bf404c5779566afd545f6
Kannan-DCI <kannanp@paragondynamics.in>
2022-03-24 22:54:52 +0530

    work order

--- src/main/java/com/bitutech/workorder/WorkOrderQueryUtil.java
+++ src/main/java/com/bitutech/workorder/WorkOrderQueryUtil.java
@@ -2,7 +2,7 @@ package com.bitutech.workorder;
 
 public class WorkOrderQueryUtil {
 
-	public static final String getWorkOrderList = "select form_code as territory,form_name as name,display_order as zipCode,module_code as addressOfCus,is_parent as organisationName from auth.form";
+	public static final String getWorkOrderList = null;
 	public static final String INSERT_WORKORDER_MASTER = null;
 
 }
