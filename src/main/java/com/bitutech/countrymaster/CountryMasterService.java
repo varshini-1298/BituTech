d8912e151bf5778256a5f7fb78923a8104f692f1
Morrisvijay <vijay@paragondynamics.in>
2022-04-12 18:07:40 +0530

    country edit service

--- src/main/java/com/bitutech/countrymaster/CountryMasterService.java
+++ src/main/java/com/bitutech/countrymaster/CountryMasterService.java
@@ -12,4 +12,6 @@ public interface CountryMasterService {
 
 	public CountryMasterResultBean deleteCountryDtl(String countryCode) throws Exception;
 
+	CountryMasterResultBean getCode(String countryMaster) throws Exception;
+
 }
