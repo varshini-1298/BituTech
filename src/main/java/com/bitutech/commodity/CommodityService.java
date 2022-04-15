61ebdc4e9ddbe715b36eac8c9ad6cd6563cb5411
Morrisvijay <vijay@paragondynamics.in>
2022-04-15 12:29:53 +0530

    commodity service

--- src/main/java/com/bitutech/commodity/CommodityService.java
+++ src/main/java/com/bitutech/commodity/CommodityService.java
@@ -2,10 +2,18 @@ package com.bitutech.commodity;
 
 import java.util.List;
 
+import com.bitutech.departmentmaster.DepartmentMasterBean;
+
 public interface CommodityService {
 
 	CommodityResultBean save(CommodityBean bean) throws Exception;
 
 	public List<CommodityBean> getCommodityList() throws Exception;
+
+	CommodityResultBean edit(String commodityCode) throws Exception;
+
+	CommodityResultBean update(CommodityBean bean) throws Exception;
+
+	CommodityResultBean delete(String commodityCode) throws Exception;
 
 }
