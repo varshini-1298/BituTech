58065c8d2eaa30d928252058350333c8d4d185cc
ishvarya227 <ishvarya227@gmail.com>
2022-05-03 18:21:19 +0530

    date picker issue cleared

--- src/main/java/com/bitutech/salescallentry/SalesEntryMasterQueryUtil.java
+++ src/main/java/com/bitutech/salescallentry/SalesEntryMasterQueryUtil.java
@@ -12,12 +12,12 @@ public class SalesEntryMasterQueryUtil {
 	public static final String UPDATE_SALESENTRY_HDR = "update salescall_hdr set customer_type=:customerType, salescall_hdr_id=:salesCallHdrId, customer=:customer,type_of_call=:typeOfCall,email_id=:emailId,mode_of_contact=:modeOfContact,designation=:designation, assign_to=:assignTo, person_met=:personMet where salescall_hdr_id=:salesCallHdrId";
 	public static final String DELETE_SALESENTRY_HDR = "delete from salescall_hdr where salescall_hdr_id=?";
 
-	public static final String INSERT_SALESENTRY_DTL = "Insert into salescall_dtl_obj (salescall_hdr_id,objective,next_call_date,status,commodity,conditions,support,not_support,remarks,created_by,created_date)"
-			+ " values (:salesCallHdrId,:objective,to_date(:nextCallDate,'YYYY-MM-DD'),:status,:commodity,:conditionSupport,:reasonSupport,:reasonNotSupport,:remarks,'E0001',now())";
+	public static final String INSERT_SALESENTRY_DTL = "Insert into salescall_dtl_obj (salescall_hdr_id,objective,salescall_obj_date,status,next_call_date,commodity,conditions,support,not_support,remarks,created_by,created_date)"
+			+ " values (:salesCallHdrId,:objective,to_date(:date,'YYYY-MM-DD'),:status,to_date(:nextCallDate,'YYYY-MM-DD'),:commodity,:conditionSupport,:reasonSupport,:reasonNotSupport,:remarks,'E0001',now())";
 
 	public static final String DELETE_SALESENTRY_DTL = "delete from salescall_dtl_obj where salescall_hdr_id=?";
 
-	public static final String SELECT_SALESENTRY_DTL = "select salescall_hdr_id as salesCallHdrId,objective as objective,to_char(next_call_date,'YYYY-MM-DD') as nextCallDate,status as status,commodity as commodity,conditions as conditionSupport,support as reasonSupport,not_support as reasonNotSupport,remarks as remarks from salescall_dtl_obj where salescall_hdr_id=? ";
+	public static final String SELECT_SALESENTRY_DTL = "select salescall_hdr_id as salesCallHdrId,objective as objective,to_char(salescall_obj_date,'YYYY-MM-DD') as date,status as status,to_char(next_call_date,'YYYY-MM-DD') as nextCallDate,commodity as commodity,conditions as conditionSupport,support as reasonSupport,not_support as reasonNotSupport,remarks as remarks from salescall_dtl_obj where salescall_hdr_id=? ";
 
 
 	
